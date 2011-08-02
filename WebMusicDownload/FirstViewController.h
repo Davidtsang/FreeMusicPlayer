eebc6beba81c859a272a7ec7a8dd6808cfc8c51a
Zen David <iceman2003cn@Zen-Davids-iMac.local>
2011-08-02 21:21:49 +0800

    Initial Commit

---
+++ WebMusicDownload/FirstViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.h
+//  WebMusicDownload
+//
+//  Created by Zen David on 8/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FirstViewController : UIViewController {
+
+}
+
+@end
---
+++ WebMusicDownload/SecondViewController.h
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.h
+//  WebMusicDownload
+//
+//  Created by Zen David on 8/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SecondViewController : UIViewController {
+    
+}
+
+@end
